34c73997842918895aaada01b8209b96c0099863
Bima Bimo <satriaabimanyu595@gmail.com>
2024-02-10 23:57:53 +0700

    add: database update after migration to railway app

--- finalproject.sql
+++ finalproject.sql
@@ -11,18 +11,35 @@
  Target Server Version : 80036 (8.0.36)
  File Encoding         : 65001
 
- Date: 09/02/2024 19:44:45
+ Date: 10/02/2024 19:31:31
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for game_types
+-- ----------------------------
+DROP TABLE IF EXISTS `game_types`;
+CREATE TABLE `game_types`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `game_id` bigint UNSIGNED NOT NULL,
+  `theme` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `game_id2`(`game_id` ASC) USING BTREE,
+  CONSTRAINT `game_id2` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of game_types
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for games
 -- ----------------------------
 DROP TABLE IF EXISTS `games`;
 CREATE TABLE `games`  (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
   PRIMARY KEY (`id`) USING BTREE
@@ -34,49 +51,26 @@ CREATE TABLE `games`  (
 INSERT INTO `games` VALUES (1, 'The Forest', 'Deskripsi Game');
 
 -- ----------------------------
--- Table structure for ratings
--- ----------------------------
-DROP TABLE IF EXISTS `ratings`;
-CREATE TABLE `ratings`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `review_id` int NULL DEFAULT NULL,
-  `user_id` int NULL DEFAULT NULL,
-  `rating` int NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `review_id`(`review_id` ASC) USING BTREE,
-  INDEX `user_id1`(`user_id` ASC) USING BTREE,
-  CONSTRAINT `review_id` FOREIGN KEY (`review_id`) REFERENCES `reviews` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `user_id1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of ratings
--- ----------------------------
-INSERT INTO `ratings` VALUES (2, 2, 3, 0);
-INSERT INTO `ratings` VALUES (3, 2, 3, 5);
-INSERT INTO `ratings` VALUES (4, 2, 3, 4);
-
--- ----------------------------
 -- Table structure for reviews
 -- ----------------------------
 DROP TABLE IF EXISTS `reviews`;
 CREATE TABLE `reviews`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `game_id` int NOT NULL,
-  `user_id` int NOT NULL,
-  `rating` int NOT NULL,
-  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `game_id` bigint UNSIGNED NULL DEFAULT NULL,
+  `user_id` bigint UNSIGNED NOT NULL,
+  `rating` bigint NULL DEFAULT NULL,
+  `comment` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `game_id`(`game_id` ASC) USING BTREE,
-  INDEX `user_id`(`user_id` ASC) USING BTREE,
+  INDEX `user_id2`(`user_id` ASC) USING BTREE,
   CONSTRAINT `game_id` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `user_id2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of reviews
 -- ----------------------------
-INSERT INTO `reviews` VALUES (2, 1, 3, 5, 'sampah');
+INSERT INTO `reviews` VALUES (2, 1, 3, 5, 'menarik');
 
 -- ----------------------------
 -- Table structure for roles
@@ -101,13 +95,15 @@ INSERT INTO `roles` VALUES (2, 'user');
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `role_id` int NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `role_id`(`role_id` ASC) USING BTREE,
+  UNIQUE INDEX `uni_users_username`(`username` ASC) USING BTREE,
+  UNIQUE INDEX `uni_users_email`(`email` ASC) USING BTREE,
   CONSTRAINT `role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
@@ -115,6 +111,5 @@ CREATE TABLE `users`  (
 -- Records of users
 -- ----------------------------
 INSERT INTO `users` VALUES (3, 'tes@gmail.com', 'bima', '$2a$10$eoY2iHIKPOk5abmCzOFWce6qodwOqGUEyy3CWFHrLJe/GnBnyiSX6', 2);
-INSERT INTO `users` VALUES (4, 'tes@gmail.com', 'bima', '$2a$10$qpAnF9hfdQfX70xxYSEYA.TGdDCfv.YC0JlSf8GFo3Etr.xJnI9kW', 2);
 
 SET FOREIGN_KEY_CHECKS = 1;
