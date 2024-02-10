3362bf3e0ec08ca0fef6a91422d0ff025cd75646
Bima Bimo <satriaabimanyu595@gmail.com>
2024-02-11 00:45:43 +0700

    main: update middleware

--- routes/route.go
+++ routes/route.go
@@ -1,14 +1,13 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "gorm.io/gorm"
+	"BACKEND/controllers"
+	middlewares "BACKEND/middleware"
 
-    "BACKEND/controllers"
-    // "BACKEND/middleware"
-
-    swaggerFiles "github.com/swaggo/files"
-    ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"gorm.io/gorm"
 )
 
 func SetupRouter(db *gorm.DB) *gin.Engine {
@@ -23,19 +22,23 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
     r.POST("/login", controllers.Login)
 
     // Game routes
-    r.POST("/games", controllers.CreateGame)
     r.GET("/games", controllers.GetGames)
-    r.PUT("/games/:id", controllers.UpdateGame)
-    r.DELETE("/games/:id", controllers.DeleteGame)
+    gameMiddlewareRoute := r.Group("/games")
+    gameMiddlewareRoute.Use(middlewares.JwtAuthMiddleware())
+    gameMiddlewareRoute.POST("/", controllers.CreateGame)
+    gameMiddlewareRoute.PATCH("/:id", controllers.UpdateGame)
+    gameMiddlewareRoute.DELETE("/:id", controllers.DeleteGame)
 
     // Review routes
-    r.POST("/reviews", controllers.CreateReview) // Tambahkan route untuk membuat review
+    r.POST("/reviews", controllers.CreateReview)
 
- // Routes untuk GameType
- r.POST("/game_types", controllers.CreateGameType)
- r.GET("/game_types/:id", controllers.GetGameType)
- r.PUT("/game_types/:id", controllers.UpdateGameType)
- r.DELETE("/game_types/:id", controllers.DeleteGameType)
+    // Routes untuk GameType
+    r.GET("/game_types/:id", controllers.GetGameType)
+    gameTypeMiddlewareRoute := r.Group("/game_types")
+    gameTypeMiddlewareRoute.Use(middlewares.JwtAuthMiddleware())
+    gameTypeMiddlewareRoute.POST("/", controllers.CreateGameType)
+    gameTypeMiddlewareRoute.PATCH("/:id", controllers.UpdateGameType)
+    gameTypeMiddlewareRoute.DELETE("/:id", controllers.DeleteGameType)
 
     
     // Swagger route
